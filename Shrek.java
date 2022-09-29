d8b06f8118adfce3d93f0f8d1eb731405428ef4e
Gabriel A. Denardi <39312459+GabrielSVS@users.noreply.github.com>
2022-09-28 23:16:22 -0300

    Update Shrek.java

--- Shrek.java
+++ Shrek.java
@@ -4,21 +4,27 @@ class Shrek {
 	    public Shrek() {
 	        cebola = 1;
 	        camada = 2.3f;
-	        //Scanner sc = new Scanner(System.in);
 	    }
-	    public int get_cebola() {
+	    public int getCebola() {
 	        return cebola;
 	    }
-	    public float get_camada() {
+	    public float getCamada() {
 	        return camada;
 	    }
-	    public void set_cebola() {
-	        cebola = 5;
+	    public void setCebola() {
+	        this.cebola = 5;
 	    }
-	    public void set_camada() {
-	        camada = 4f;
+	    public void setCamada() {
+	        this.camada = 4f;
 	    }
 	    public float soma() {
 	        return cebola+camada;
 	    }
 	}
+
+/* 
+A classe deve conter atributos de pelo menos 2 tipos diferentes (algumas possibilidades: String, int, double, boolean, etc.).
+A classe deve conter um construtor (pode haver mais de um, com diferentes argumentos).
+A classe deve conter métodos com prefixo get e set, respectivamente para obter e alterar cada atributo.
+A classe deve conter um método que implemente alguma operação com um ou mais atributos (por exemplo, um teste de condição, um cálculo, uma operação com strings, etc.). 
+*/
